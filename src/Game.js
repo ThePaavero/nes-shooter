4eee8b6719552171ad60df02c0904ef00605dda2
Pekka Siiriäinen <pekka.siiriainen@mirumagency.com>
2020-01-04 21:54:54 +0200

    Reverted back to shield picture without the "bling."

--- src/Game.js
+++ src/Game.js
@@ -578,9 +578,9 @@ const Game = (playground) => {
     const image = playground.images[state.player.shieldHealth > 0 ? 'shield' : 'shieldBroken']
     playground.layer.drawImage(image, state.player.x - 5, state.player.y - 10, 37, 21)
     playground.layer.fillStyle('#fff')
-    playground.layer.font(`10px PixelEmulatorxq08`)
+    playground.layer.font(`7px PixelEmulatorxq08`)
     const text = state.player.shieldHealth < 0 ? 'X' : Math.round(state.player.shieldHealth * 10)
-    playground.layer.fillText(text, state.player.x - 10, state.player.y - 7)
+    playground.layer.fillText(text, state.player.x - 13, state.player.y - 7)
   }
 
   const handleAspectRatio = () => {
